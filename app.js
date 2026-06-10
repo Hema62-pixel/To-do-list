9a27fa44b9e906893708f24a8f9b824753bfca6a
Hema62-pixel <hemathirumoorthy95@gmail.com>
2026-06-10 18:13:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ function saveTodos(){
   localStorage.setItem(
     STORAGE_KEY,
     JSON.stringify(state.todos)
-    );
+    ); 
 }
 
 function addTodo(text){
@@ -30,3 +30,88 @@ function addTodo(text){
   saveTodos();
   render();
 }
+
+
+function editTodo(id){
+  const todo = state.todos.find(t=>t.id===id);
+
+  const newText=prompt("Edit task:",todo.text);
+
+  if(!newText) return;
+
+  todo.text=newText;
+
+  saveTodos();
+  render();
+}
+
+function toggleTodo(id){
+  const  todo = state.todos.find(t=>t.id===id);
+   todo completed=!todo completed;
+  saveTodos();
+  render();
+}
+
+function deleteTodo(id){
+  state.todos=state.todos.filter(
+    todo=>todo.id!==id
+    );
+  saveTodos();
+  render();
+}
+
+
+function getFilteredTodos(){
+  switch(state.filter){
+      case"active";
+      return state.todos.filter(
+        todo=>todo.completed
+        );
+    default:
+      return state.todos;
+  }
+}
+
+const list = document.getElementById("todo-list");
+
+function render(){
+  list.innerHTML ="";
+   const todos=getFilteredTodos();
+
+  todosforEach(todo=>{
+    const li=document.createElement("li");
+
+    li.dataset.id=todo.id;
+
+    if(todo.completed){
+      li.classList.add("completed");  
+    }
+
+    li.innerHTML="";
+
+    const todos = getFilteredTodos();
+
+    todos.forEach(todo =>{
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
